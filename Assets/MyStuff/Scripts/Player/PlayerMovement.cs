f1bc58f219d958fd66c8cd9423941202fb0d6ffb
Eric Oudin <eric_oudin29@yahoo.com>
2022-03-16 17:05:52 -0400

    client movement script

--- Assets/MyStuff/Scripts/Player/PlayerMovement.cs
+++ Assets/MyStuff/Scripts/Player/PlayerMovement.cs
@@ -4,58 +4,71 @@ using UnityEngine.AI;
 public class PlayerMovement : MonoBehaviour
 {
     //player's move agent
-    internal NavMeshAgent agent;
+    internal PlayerBrain brain;
+	internal Animator animator;
 
-    //DestinationMarker
-    public GameObject destinationMarkerPrefab;
+	//DestinationMarker
+	public GameObject destinationMarkerPrefab;
     
     //currently placed destinationMarker
     internal GameObject destinationMarkerPlaced;
 
+	private bool isMoving;
+
     // Start is called before the first frame update
     void Start()
     {
-        agent = gameObject.GetComponent<NavMeshAgent>();
+		brain = GetComponent<PlayerBrain>();
+		animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-		NavMeshPath path = agent.path;
-        if (GetComponent<PlayerBrain>() == GameManager.Instance.ClientPlayer)
-        {
-            //sets destination for navmesh and creates marker
-            if (Input.GetMouseButton(0) && !GameManager.Instance.UsingUI && !GameManager.Instance.DraggingObject)
-            {
-                RaycastHit hit;
-
-                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100, LayerMask.GetMask("Ground")))
-                {
-
-                    if (destinationMarkerPlaced != null)
-                        Destroy(destinationMarkerPlaced.gameObject);
-
-                    destinationMarkerPlaced = Instantiate(destinationMarkerPrefab, hit.point, Quaternion.identity);
-                    agent.destination = hit.point;
-                }
-            }
-
-            //Destroys destination marker if close
-            if (agent.remainingDistance <= 0.1f)
-            {
-                Destroy(destinationMarkerPlaced);
-            }
-        }
-    }
-
-    internal void StopPlayerFromMoving()
+	// Update is called once per frame
+	void FixedUpdate()
 	{
-        Destroy(destinationMarkerPlaced);
-        agent.destination = gameObject.transform.position;
+		if (brain.PathPoints != null && brain.PathPoints.Count > 0)
+		{
+			if (!isMoving)
+			{
+				isMoving = true;
+				animator.SetBool("IsWalking", isMoving);
+			}
+
+			if (destinationMarkerPlaced != null)
+			{
+				var destinationPoint = brain.PathPoints.Last();
+				destinationMarkerPlaced = Instantiate(destinationMarkerPrefab, destinationPoint, Quaternion.identity);
+			}
+
+			var nextPoint = brain.PathPoints.Peek();
+			// move towards point
+			if (Vector3.Distance(transform.position, nextPoint) > 0.1f)
+			{
+				Vector3.MoveTowards(transform.position, nextPoint, Speed * Time.fixedDeltaTime);
+				Destroy(destinationMarkerPlaced);
+			}
+			else
+			{
+				brain.PathPoints.Dequeue();
+			}
+		}
+		else if (isMoving)
+		{
+			isMoving = false;
+			animator.SetBool("IsWalking", isMoving);
+		}
+
+
+		//Destroys destination marker if close
+		if (Vector3.Distance(transform.position, destinationMarkerPlaced.transform.position) <= 0.1f)
+		{
+			Destroy(destinationMarkerPlaced);
+		}
 	}
 
-    public void SetDestination(Vector3 newDest) 
-    {
-        agent.destination = newDest;
-    }
+
+	internal void StopPlayerFromMoving()
+	{
+        Destroy(destinationMarkerPlaced);
+		brain.PathPoints = null;
+	}
 }
