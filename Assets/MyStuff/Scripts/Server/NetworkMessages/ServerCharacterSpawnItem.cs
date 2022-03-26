eed13e84c3abdbf1ffb26e8587110119bcbd3544
Eric Oudin <Eric_Oudin29@yahoo.com>
2022-03-26 17:07:07 -0400

    player prefab updates

--- Assets/MyStuff/Scripts/Server/NetworkMessages/ServerCharacterSpawnItem.cs
+++ Assets/MyStuff/Scripts/Server/NetworkMessages/ServerCharacterSpawnItem.cs
@@ -3,46 +3,46 @@ using System.Linq;
 
 namespace Server.Networking
 {
-    public class ServerCharacterSpawnItem : IServerHandle
-    {
-        string[] AdminNames = new string[] {"yo", "CrimOudin", "Syzzurpp"};
+	public class ServerCharacterSpawnItem : IServerHandle
+	{
+		string[] AdminNames = new string[] { "yo", "CrimOudin", "Syzzurpp" };
 
-        public int GetMessageId()
-        {
-            return (int)Packets.CharacterSpawnItem;
-        }
+		public int GetMessageId()
+		{
+			return (int)Packets.CharacterSpawnItem;
+		}
 
-        public void ReadMessage(int _fromClientId, Packet _packet)
-        {
-            string itemInternalName = _packet.ReadString();
+		public void ReadMessage(int _fromClientId, Packet _packet)
+		{
+			string itemInternalName = _packet.ReadString();
 
-            ThreadManager.ExecuteOnMainThread(() =>
-            {
-                ServerPlayerBrain character = ServerGameManager.Instance.GetPlayer(_fromClientId);
+			ThreadManager.ExecuteOnMainThread(() =>
+			{
+				ServerPlayerBrain character = ServerGameManager.Instance.GetPlayer(_fromClientId);
 
-            if (AdminNames.Any(x => x.Equals(character.characterName, System.StringComparison.OrdinalIgnoreCase)))
-                {
-                    int? slotIndex = character.GetComponent<ServerPlayerInventory>().FindFirstOpenItemSlot();
+				if (AdminNames.Any(x => x.Equals(character.characterName, System.StringComparison.OrdinalIgnoreCase)))
+				{
+					int? slotIndex = character.GetComponent<ServerPlayerInventory>().FindFirstOpenItemSlot();
 
-                    if (slotIndex != null)
-                    {
-                        character.GetComponent<ServerPlayerInventory>().AddToServerInventory(slotIndex.Value, itemInternalName);
-                        WriteMessage(_fromClientId, character, slotIndex.Value, itemInternalName);
-                    }
-                }
-            });
-        }
+					if (slotIndex != null)
+					{
+						character.GetComponent<ServerPlayerInventory>().AddToServerInventory(slotIndex.Value, itemInternalName);
+						WriteMessage(_fromClientId, character, slotIndex.Value, itemInternalName);
+					}
+				}
+			});
+		}
 
-        public void WriteMessage(int clientId, ServerCharacterBrain character, int slotIndex, string itemInternalName)
-        {
-            using (Packet _packet = new Packet(GetMessageId()))
-            {
-                _packet.Write(character.GetInstanceID());
-                _packet.Write(slotIndex);
-                _packet.Write(itemInternalName);
+		public void WriteMessage(int clientId, ServerCharacterBrain character, int slotIndex, string itemInternalName)
+		{
+			using (Packet _packet = new Packet(GetMessageId()))
+			{
+				_packet.Write(character.GetInstanceID());
+				_packet.Write(slotIndex);
+				_packet.Write(itemInternalName);
 
-                ServerSend.SendTcpDataAuthenticated(clientId, _packet);
-            }
-        }
-    }
+				ServerSend.SendTcpDataAuthenticated(clientId, _packet);
+			}
+		}
+	}
 }
